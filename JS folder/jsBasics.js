ee19e1a32dea75354b2f9f0950a7d14b1d870da0
Jyoti Rani <jyoti35994@yahoo.com>
2021-07-30 17:26:21 -0700

    Java script - print 'hello world'

---
+++ JS folder/jsBasics.js
@@ -0,0 +1,15 @@
+//3 ways to write  scripts 
+// inline,internal & External .
+
+//document.write('Hello world');
+//alert('Hello world');
+//console.log('Hello world');
+
+//it display the text on web page.
+document.write({name: 'jyoti'})
+
+//it gives a alert window with specfic text that you wrote .
+alert('hello world');
+
+//it display the output in consol window of web page --right click>inspect>console--  it is more powerful then other 2  .
+console.log('hello world');
