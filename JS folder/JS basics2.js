5d8a9e1972fae2dcc66091fdd5f19d2a79bbed77
Jyoti Rani <jyoti35994@yahoo.com>
2021-07-30 18:03:03 -0700

    Java script - Statements , Comments

---
+++ JS folder/JS basics2.js
@@ -0,0 +1,13 @@
+//statments - set of instructions .
+//comment - shorcut command +/
+
+//single line comment
+/*multi
+line
+comment*/
+
+
+
+//multi statements can be written by adding  semi colon ; 
+console.log ("hello People");console.log ("hello Jyoti");console.log ("hello birds");console.log ("hello beautiful");
+
