06021194d8cfc64ff26593a2a630494d0c7b0e79
Jyoti Rani <jyoti35994@yahoo.com>
2021-08-02 13:41:47 -0700

    JS - Variables ( LET/CONST/VAR )

---
+++ JS folder/js variableAssignment.js
@@ -0,0 +1,18 @@
+// Variable - LET vs CONST vs VAR
+//Const value cannot be re assigned .
+
+//Var
+var myName = 'Neenu';
+console.log(myName);
+
+//let
+let $color = "pink";
+console.log($color);
+
+//Const
+
+const _Country  = 'Canada';
+console.log(_Country);
+
+//If you want to change variable of CONST type  ,console will show error .
+_Country = 'Italy';
