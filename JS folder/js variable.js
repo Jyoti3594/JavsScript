5dba50295fa2fdecf5cc99b98d9a92de3d7571cb
Jyoti Rani <jyoti35994@yahoo.com>
2021-08-01 23:05:28 -0700

    JS - Variables (Store ,Access ,Modify existing & Syntex)

---
+++ JS folder/js variable.js
@@ -0,0 +1,26 @@
+//Variables - Basic Building block 
+//Variables - Store ,Access ,Modify == value
+//Declare, Assignment Operator , Assign value
+
+
+
+//Syntex of variable : 
+// let VariableName =  "---";
+//console.log(variableName)
+
+let myName="Jyoti Rani";
+console.log(myName);
+//Just assign variable name
+let myAddress ,zipCode, city;
+//assign value later ,  
+myAddress = "10 MainStreet";
+zipCode = "XYZ-ZYX";
+city = "Vancouver" ;
+
+//modify existing
+myName = "Neenu" ; 
+
+console.log(myAddress , zipCode , city);
+console.log(myName);
+
+
