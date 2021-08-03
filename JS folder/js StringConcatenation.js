1f84c28c684a5cab4fc9b5294ba8bb572413507b
Jyoti Rani <jyoti35994@yahoo.com>
2021-08-03 13:23:07 -0700

    JS - String concatenation

---
+++ JS folder/js StringConcatenation.js
@@ -0,0 +1,16 @@
+//String Concatenation : it combine the string values . 
+// add ' ' space in between words if want to .
+
+
+
+//expample 1 
+const website = 'youtube';
+const url = 'https://wwww.'+ website + '.com';
+console.log(url);
+
+
+//expample 2
+let initialN = 'Jyoti';
+let lastName = 'Rani';
+const fullName =  initialN + " " + lastName;
+console.log('My full name is' + ' ' + fullName);
