fc851247bb0d3b3059833d7eb5080b0b3516b94b
Jyoti Rani <jyoti35994@yahoo.com>
2021-08-07 13:47:48 -0700

    JS-   Operators (+= ,-= ,*= ,/= ,-- , ++ ,%)

---
+++ JS folder/js operator.js
@@ -0,0 +1,40 @@
+//operator :  += ,-=, /= ,*= , ++ , -- , % 
+
+// Modulus (%) operator return the reminder  after ther integer division  .
+
+
+//+= will add number to existing value assigned .
+let myNumber = 50;
+myNumber+= 5;
+console.log(myNumber);
+
+// -= 
+let myNumber2 = 50;
+myNumber2-= 5;
+console.log(myNumber2);
+
+//"/="
+let myNumber3 = 50;
+myNumber3/= 5;
+console.log(myNumber3);
+
+//*=
+let myNumber4 = 50;
+myNumber4*= 5;
+console.log(myNumber4);
+
+//++  it will add 1 to assigned value .
+let myNumber5 = 50;
+myNumber5++;
+console.log(myNumber5);
+
+//-- it will minus 1 from given number
+let myNumber6 = 50;
+myNumber6--;
+console.log(myNumber6);
+
+//% it will give reminder after division
+const pizzaSlice = 10;
+const children =4;
+const amount = pizzaSlice%children;
+console.log(amount);
