e0afb523932b60eb392cbe736fca77d239bbc287
Jyoti Rani <jyoti35994@yahoo.com>
2021-08-06 20:03:43 -0700

    JS-  Variable(Numbers)

---
+++ JS folder/JS Numbers.js
@@ -0,0 +1,27 @@
+//Numbers
+//Loosely Typed = don't declare type .
+
+const myNumber = 12;
+const myNumber2 = 23.65;
+
+
+
+console.log(myNumber);
+console.log(myNumber2);
+
+//Addition
+const sum =myNumber+myNumber2;
+console.log(sum);
+
+//Subtraction
+const minus =myNumber-myNumber2;
+console.log(minus);
+
+
+//Multiply
+const multiPly =myNumber*myNumber2;
+console.log(multiPly);
+
+//division
+const divide =myNumber/myNumber2;
+console.log(divide);
