0b19efbf6f688a7e991ab266607916d86c75766b
Jyoti Rani <jyoti35994@yahoo.com>
2021-08-08 01:05:58 -0700

    JS - Data Type : Primitive .

---
+++ JS folder/js primitiveDataType.js
@@ -0,0 +1,34 @@
+/*Data Types :
+
+1) Primitive-
+String,Number,Boolean,Null,Undefined,Symbol
+
+2) Object-
+Arrays,Functions,Objects
+
+//typeof - operator (typeof variable) , (typeof value) */
+
+
+//string
+const text = "lorem";
+
+//number
+const num = 4;
+
+//boolean
+let result1=true;
+let result2=false;
+
+//Null  :shown as object in console  it is JS error .
+const data = null;
+
+//undegfined
+let result3 ;
+
+//type of -operator(type of variable)
+console.log(typeof text);
+console.log(typeof num);
+console.log(typeof result1);
+console.log(typeof result2);
+console.log(typeof data);
+console.log(typeof result3);
