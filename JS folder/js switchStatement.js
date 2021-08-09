7ee29b5d27f9690845fa3f6853020545e8297ee0
Jyoti Rani <jyoti35994@yahoo.com>
2021-08-09 00:01:31 -0700

    JS - Switch statement

---
+++ JS folder/js switchStatement.js
@@ -0,0 +1,28 @@
+//Switch statement
+
+//example - dice : 1-6
+const dice = 6;
+
+switch (dice){
+    case 1:
+    console.log('you got One..');
+    break;
+    case 2:
+    console.log('you got two..');
+    break;
+    case 3:
+    console.log('you got three..');
+    break;
+    case 4:
+    console.log('you got four..');
+    break;
+    case 5:
+    console.log('you got five..');
+    break;
+    case 6:
+    console.log('you got SIX !!');
+    break;
+    default:
+    console.log('You did not roll the dice  :( ');
+    break;
+}
