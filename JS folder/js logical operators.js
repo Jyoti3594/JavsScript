a28fcbe4e06618932ca7802cff488343f6f3711b
Jyoti Rani <jyoti35994@yahoo.com>
2021-08-08 23:44:31 -0700

    JS -  Logical Operators

---
+++ JS folder/js logical operators.js
@@ -0,0 +1,23 @@
+// Logical Operators 
+// (|| - OR ), (&& - AND) , (!)
+
+const userName = 'Mickey';
+const age = 24;
+const eyes = 'brown';
+const height = 5.5;
+
+// OR & !
+if(userName!== 'Mickey' ||  age===24){
+    console.log('hello there, Mickey :D ');
+}
+else {
+    console.log('wrong  User');
+}
+
+//AND
+if(eyes=== 'brown' &&  height===5.5){
+    console.log('Your eyes are pretty!!');
+}
+else {
+    console.log('Hi...');
+}
