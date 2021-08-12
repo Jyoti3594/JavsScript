94e87d56f5ca1f94e2242290173189767d111992
Jyoti Rani <jyoti35994@yahoo.com>
2021-08-12 00:26:29 -0700

    JS - Unary & Binary  & Ternary operators

---
+++ JS folder/js UnaryBinaryOperators.js
@@ -0,0 +1,22 @@
+//Unary - Binary Operators
+
+//Unary operator - typeof
+let text ='some text';
+console.log(typeof text);
+
+//binary Operators
+let number = 3;
+let number2 = 2+5;
+
+const value = 2>3;
+/*if(value{
+    console.log('value is true');
+})
+else{
+    console.log('value is false');
+} */
+
+
+//ternary operator 
+//format : condition ? (run if true):(run if false)
+value ? console.log('value is true'):console.log('value is false');
