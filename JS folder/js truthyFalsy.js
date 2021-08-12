19794805f726c79e92587e7629592494e6deafd6
Jyoti Rani <jyoti35994@yahoo.com>
2021-08-12 00:09:34 -0700

    JS -  Truthy & Falsey

---
+++ JS folder/js truthyFalsy.js
@@ -0,0 +1,17 @@
+//Truthy & Falsy
+//""  , '' , `` , 0 ,-0 , NaN ,false,Null ,Undefined 
+
+
+const bool1 = true;
+const bool2= 2>1;
+
+const text = 'text';
+
+if(text){
+    console.log('hey the value Truthy');
+}
+else{
+    console.log('hey the value falsy');
+}
+
+ 
