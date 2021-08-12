ef9591675df7580e325cac7a81668049901fbedd
Jyoti Rani <jyoti35994@yahoo.com>
2021-08-11 23:48:11 -0700

    JS - Null & Undefined

---
+++ JS folder/js NullVSundefined.js
@@ -0,0 +1,11 @@
+//Null & Undefined
+//both represent "No value"
+
+
+//Undefined - "javascript can not find the values"
+//Null - "Set by developers "
+
+let number = 20 + null;
+console.log(number);
+let number2 = 20 + undefined;
+console.log(number2);
