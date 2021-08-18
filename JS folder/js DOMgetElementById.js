62bd4c6a457608b947c8c8de12ae0e96cf0abb2c
Jyoti Rani <jyoti35994@yahoo.com>
2021-08-17 20:26:40 -0700

    JS - DOM - SelectElementById

---
+++ JS folder/js DOMgetElementById.js
@@ -0,0 +1,10 @@
+//getElementById('element')
+//Select the element or group of the elements the we want .
+//decide the effect we want to apply to the selection.
+
+const h1 = document.getElementById('tittle');
+h1.style.color = 'red';
+
+const btn  = document.getElementById('btn');
+btn.style.color=" white";
+btn.style.backgroundColor='blue'; 
