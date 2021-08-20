f8b9daded3c761ac6c22d0ba8b255556c468798b
Jyoti Rani <jyoti35994@yahoo.com>
2021-08-20 12:48:58 -0700

    JS - NodeValue & TextContent

---
+++ JS folder/js nodeValue Textcontent.js
@@ -0,0 +1,12 @@
+//both return text
+
+//Node value
+const item = document.getElementById('special');
+const value = item.firstChild.nodeValue;
+console.log(value);
+
+
+
+//Text Content
+const easyValue = item.textContent;
+console.log(easyValue);
