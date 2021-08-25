9a6fc6383192c6735444c811d168f6448dd12421
Jyoti Rani <jyoti35994@yahoo.com>
2021-08-25 13:08:01 -0700

    JS -  Css style VS ClassList

---
+++ JS folder/js ClassListvsCSSstyle.js
@@ -0,0 +1,14 @@
+//CSS
+
+const randomE = document.querySelector('.random');
+// console.log(random.style); -disadvange :  adding property one by one .
+
+
+//random.style.background = 'blue';
+//random.style.color = 'white';
+//random.style.fontSize = '3rem';
+//random.style.textTransform = 'capitalized';
+
+//ClassList :    accessing by 1 line of code 7 faster
+
+randomE.classList.add('tittle');
